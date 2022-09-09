a4757c763ccb966a889037b431e1074938dfbba0
rodrigo prado <pradorodrigo19@gmail.com>
2022-09-09 19:57:58 +0000

    Parei no meio da aula 5 sobre leitura de caracteres do canal Programe seu futuro no youtube

---
+++ Aulas/Aula003/aula.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("Hello word!\n");
+	
+	printf("\nPrecione qualquer tecla para finalizar.\n");
+	getchar(); //Faz a leitura de caracter
+	
+	return 0;
+}
---
+++ Aulas/Aula004/aula.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	int valor;
+	
+	printf("Digite um valor:");
+	scanf("%d", &valor); //Recebe valore digitados pelo usuario (Tipo do valor, espasso onde ira sauvalo)
+	
+	printf("\nValor: %d\n", valor);
+	
+	return 0;
+}
